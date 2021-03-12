8f37fa4b8aa4ea72efc278b96609baf5285aa5d2
Eric Mill <eric@konklone.com>
2021-03-12 07:27:40 -0800

    Updating Node and Gulp for v12 (#263)
    * update gulp to v4 and node to v12
    
    * update bundler to 2.2

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,29 @@
 
 const path = require('path');
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const exec = require('child_process').exec;
 
 const PORT = process.env.PORT || '8000';
+
 const USWDS_DIST = 'node_modules/uswds/dist';
 const USWDS_DIST_DIR = path.join(__dirname, ...USWDS_DIST.split('/'));
 
+const PROJECT_SASS_SRC = "./assets/sass/**/*.scss";
+const PROJECT_CSS_DEST = './assets/css'
+
+const PROJECT_ASSETS_SRC = `${USWDS_DIST}/@(js|fonts|img)/**/**`
+const PROJECT_ASSETS_DEST = './assets/vendor/uswds'
+
+
 gulp.task('copy-uswds-assets', () => {
-  return gulp.src(`${USWDS_DIST}/@(js|fonts|img)/**/**`)
-  .pipe(gulp.dest('./assets/vendor/uswds'));
+  return gulp.src(PROJECT_ASSETS_SRC)
+    .pipe(gulp.dest(PROJECT_ASSETS_DEST));
 });
 
-gulp.task('sass', () => {
-  return gulp.src('./assets/sass/**/*.scss')
+gulp.task('build-sass', () => {
+  return gulp.src(PROJECT_SASS_SRC)
     .pipe(sourcemaps.init())
     .pipe(sass({
       includePaths: [
@@ -25,11 +32,15 @@ gulp.task('sass', () => {
       ]
     }).on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest(PROJECT_CSS_DEST));
 });
 
-gulp.task('watch', ['default'], () => {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+/** entry points **/
+
+// gulp.task('init', gulp.series('copy-uswds-assets'));
+
+gulp.task('watch', () => {
+  gulp.watch(PROJECT_SASS_SRC, gulp.series('copy-uswds-assets','build-sass'));
 });
 
-gulp.task('default', ['sass', 'copy-uswds-assets']);
+gulp.task('default', gulp.series('copy-uswds-assets', 'build-sass'));
